4b2b9f1a2c4873a8d7f09467ba4b0461d37862cc
jrdeveloper2003 <jrdeveloper2003@gmail.com>
2024-07-14 10:43:45 +0000

    tASK7 cREATED

---
+++ task7/android/app/src/main/kotlin/com/example/task7/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ task7/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:task7/Login.dart';
+
+void main(List<String> args) {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Login(),
+    );
+  }
+}
